9d577c428ce7b450716564d0a8a29bd1ac884658
Renan Gimenes Musardo <renan.gimenes@gmail.com>
2022-10-10 10:29:10 -0300

    Adicionar arquivos de projeto.

---
+++ API/Controllers/ValuesController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("OK: " + DateTime.Now);
+        }
+    }
+}
---
+++ API/Models/Value.cs
@@ -0,0 +1,24 @@
+﻿namespace API.Models
+{
+    public class Value
+    {
+    }
+
+    public class Blog
+    {
+        public int BlogId { get; set; }
+        public string Name { get; set; }
+
+        public virtual List<Post> Posts { get; set; }
+    }
+
+    public class Post
+    {
+        public int PostId { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+
+        public int BlogId { get; set; }
+        public virtual Blog Blog { get; set; }
+    }
+}
